aa25c08612c322342b70b05d16ac26104191ea3a
Sayan Ghosh <sgic2001@gmail.com>
2024-06-18 15:12:00 +0530

    add home to login redirection

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect('/login')
 
 @app.route('/login')
 def login():
