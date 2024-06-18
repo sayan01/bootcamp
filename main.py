67447bca4b30bf8141c1b9d9e6e45c88115fc5cc
Sayan Ghosh <sgic2001@gmail.com>
2024-06-18 16:48:48 +0530

    add homepage and user session

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from dotenv import load_dotenv
 import os
 
@@ -6,18 +6,24 @@ load_dotenv()
 
 app = Flask(__name__)
 
+secret_key = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = secret_key
+
 @app.route('/')
 def home():
-    return redirect('/login')
+    username=session['username']
+    return render_template('index.html',user_name=username, names=
+                           ['Alpha', 'Bravo', 'Charlie', 'Jack'])
 
 @app.route('/login')
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST'])
 def login_post():
     username = request.form.get('username')
     password = request.form.get('password')
-    return username+password
+    session['username'] = username
+    return redirect('/')
 
 # app.run(debug=True)
