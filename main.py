d708da34135001da66b092e9db88294f48143962
Sayan Ghosh <sgic2001@gmail.com>
2024-06-19 11:14:48 +0530

    one to many relationship

--- main.py
+++ main.py
@@ -1,18 +1,56 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+
+# config
 
 load_dotenv()
 
 app = Flask(__name__)
 
-secret_key = os.getenv('SECRET_KEY')
-app.config['SECRET_KEY'] = secret_key
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+# models
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, nullable=False)
+    passhash = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(50), nullable=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    description = db.Column(db.String(250), nullable=True)
+
+    products = db.relationship('Product', backref='category', lazy=True, cascade='all, delete-orphan')
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(15), nullable=False)
+    description = db.Column(db.String(250), nullable=True)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    dom = db.Column(db.Date, nullable=True)
+    best_before = db.Column(db.Integer, nullable=True)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+
+
+
+
+# routes
 
 @app.route('/')
 def home():
     username=session['username']
-    return render_template('index.html',user_name=username, names=
+    return render_template('index.html',
+                           var1=os.getenv('FLASK_APP'),
+                           user_name=username, names=
                            ['Alpha', 'Bravo', 'Charlie', 'Jack'])
 
 @app.route('/login')
